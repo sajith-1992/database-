663d6884b432fd538390322edffc476356102ccc
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2023-03-03 20:02:40 -0500

    create cart %%%

--- routes/admin.js
+++ routes/admin.js
@@ -17,9 +17,12 @@ router.get('/', function(req, res, next) {
   
 });
 router.get('/add-product',(req,res)=>{
+
   res.render('./admin/add-product')
 })
 router.post('/add-product',(req,res)=>{
+  console.log("add product");
+
   // console.log(req.body);
   // console.log(req.files.Image)
   // console.log(req.files.image);
